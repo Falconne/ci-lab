11cfdcb7cf838bb780c0f65f9957d47cc3d73667
Anuradha Dissanayake <falconne@gmail.com>
2026-02-27 21:43:07 +1300

    Todo

--- src/be/Mergician/Entities/BranchRefreshRequest.cs
+++ src/be/Mergician/Entities/BranchRefreshRequest.cs
@@ -1,5 +1,11 @@
 namespace Mergician.Entities;
 
+// TODO: Get rid of this class. The frontend should not be requesting refreshes with all this detail. Refactor the code
+// so that the frontend just sends `DashboardPollRequest` everywhere and the backend can look up what it needs from the
+// database. LastUpdated and MergeGroupId need not be sent by the frontend. When responding to a refresh-activity poll,
+// the last updated time for a branch should be determined by a Gitlab API call, just like approvals status is done,
+// and returned as part of the activity refresh info.
+
 /// <summary>
 ///     Request to refresh the MR/approval status for a specific branch in a project.
 /// </summary>
