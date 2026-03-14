25b43dac2b9be7ac096ae6a83afd99c63e6e6846
Anuradha Dissanayake <falconne@gmail.com>
2026-03-15 11:36:01 +1300

    Add util project

---
+++ src/be/Util/EnumerableExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Util;
+
+// TODO: Check all the C# code in this repo where the utility methods in this class can be used
+
+public static class EnumerableExtensions
+{
+    public static bool NotAny<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+    {
+        return !source.Any(predicate);
+    }
+}
---
+++ src/be/Util/Extensions.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace Util;
+
+// TODO: Check all the C# code in this repo where the utility methods in this class can be used
+// and apply them. Specifically, replace string IsNullOrWhiteSpace checks with IsEmpty and IsNotEmpty.
+
+public static class Extensions
+{
+    public static bool ContainsIgnoringCase(this string? str, string compareTo)
+    {
+        return str != null && str.ToLower().Contains(compareTo.ToLower());
+    }
+
+    public static bool EqualsIgnoringCase(this string str, string compareTo)
+    {
+        return str.Equals(compareTo, StringComparison.InvariantCultureIgnoreCase);
+    }
+
+    public static bool EqualsIgnoringCaseAndSpace(this string str, string compareTo)
+    {
+        return str.Trim().Equals(compareTo.Trim(), StringComparison.InvariantCultureIgnoreCase);
+    }
+
+    public static bool IsEmpty([NotNullWhen(false)] this string? str)
+    {
+        return string.IsNullOrWhiteSpace(str);
+    }
+
+    public static bool IsNotEmpty([NotNullWhen(true)] this string? str)
+    {
+        return !string.IsNullOrWhiteSpace(str);
+    }
+
+    public static bool IsASCII(this string value)
+    {
+        // ASCII encoding replaces non-ascii with question marks, so we use UTF8 to see if multi-byte sequences are there
+        return Encoding.UTF8.GetByteCount(value) == value.Length;
+    }
+
+    public static IEnumerable<(T item, int index)> WithIndex<T>(this IEnumerable<T> source)
+    {
+        return source.Select((item, index) => (item, index));
+    }
+
+    public static bool IsEmpty([NotNullWhen(false)] this Array? array)
+    {
+        return array == null || array.Length == 0;
+    }
+
+    public static bool IsNotEmpty([NotNullWhen(true)] this Array? array)
+    {
+        return array is { Length: > 0 };
+    }
+}
